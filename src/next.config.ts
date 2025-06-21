9c182c61cab3939bb138361314a77657f90a71df
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-06-21 16:12:20 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,73 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false, // Changed from true
+  },
+  eslint: {
+    ignoreDuringBuilds: false, // Changed from true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.grofers.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'freepngimg.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'banner2.cleanpng.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'shop.mtrfoods.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com', // Added new domain
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'icon2.cleanpng.com', // Added new domain
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com', // Added for the new banner image
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sdmntprwestus.oaiusercontent.com', // Added for the new banner image
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.clevelandclinic.org',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
