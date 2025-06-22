2cfc8010df7974ea0e685c521777c33ac3f31b38
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-06-22 02:18:32 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -14,67 +14,56 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'cdn.grofers.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'freepngimg.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'img.freepik.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'banner2.cleanpng.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'shop.mtrfoods.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com', // Added new domain
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'icon2.cleanpng.com', // Added new domain
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com', // Added for the new banner image
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'sdmntprwestus.oaiusercontent.com', // Added for the new banner image
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'assets.clevelandclinic.org',
-        port: '',
         pathname: '/**',
       }
     ],
