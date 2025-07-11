edf165d70bd382c35572b0afb7885728824b582a
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-07-11 03:17:22 +0000

    During the login or forgot password process, hide the top navigation hea

--- src/components/shared/AppProvider.tsx
+++ src/components/shared/AppProvider.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { Toaster } from "@/components/ui/toaster";
@@ -7,7 +8,8 @@ import { usePathname } from 'next/navigation';
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const isAuthPage = pathname.startsWith('/auth');
+  const authRoutes = ['/login', '/signup', '/forgot-password'];
+  const isAuthPage = authRoutes.includes(pathname);
   const isSearchPage = pathname === '/search';
 
   return (
